9e443afb2763eac8eec2ff44bf2976d809c21057
ahshasib <154332574+ahshasib@users.noreply.github.com>
2025-08-30 23:58:30 +0600

    change nav style

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -12,15 +12,13 @@ const Navbar = () => {
         <ul
           tabIndex="0"
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
+          <li><a>Home</a></li>
+        <li>
+          Product
+        </li>
+        <li><a>About</a></li>
+        <li><a>Blog</a></li>
+        <li><a>Contact</a></li>
         </ul>
       </div>
       <a className="btn btn-ghost text-xl">daisyUI</a>
@@ -33,6 +31,7 @@ const Navbar = () => {
         </li>
         <li><a>About</a></li>
         <li><a>Blog</a></li>
+        <li><a>Contact</a></li>
       </ul>
     </div>
     <div className="navbar-end">
